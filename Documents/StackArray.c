b09be333f865e8dab390b2d50b263961981e312f
murlgangaram <165001332+murlgangaram@users.noreply.github.com>
2024-06-13 16:11:32 +0800

    Add files via upload

---
+++ Documents/StackArray.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+	StackArrayList s;
+    int elem;
+
+    initStack(&s);
+    stack_push(&s, 5);
+    stack_push(&s, 10);
+    stack_push(&s, 15);
+    display(s);
+//    visualize(s);
+	
+ 	stack_pop(&s);
+    stack_pop(&s);
+    stack_pop(&s);
+    
+	return 0;
+}
---
+++ Documents/StackArray.h
@@ -0,0 +1,87 @@
+#ifndef STACK_ARRAY
+#define STACK_ARRAY
+
+#include <stdbool.h>
+
+#define MAX 1
+
+typedef struct{
+	int data[MAX];
+	int top;
+}StackArrayList;
+
+void initStack(StackArrayList *s){
+	s->top = -1;
+}
+StackArrayList createStack(){
+	StackArrayList *s;
+	s->top = -1;
+	
+	return s;
+	
+}
+
+bool stack_push(StackArrayList *s, int elem){
+	
+	if(s->top < MAX -1){
+		s->top++;
+		s->elem[s->top] = elem;
+		
+		return true;
+	}
+	return false;
+}
+bool stack_pop(StackArrayList *s){
+	if(s->top > -1){
+		s->top--;
+		
+		return true;
+	}
+	
+	return false;
+}
+bool stack_peek(StackArraylist *s){
+	
+	return (s->top != -1) ? s->data[s->top] : -1;
+}
+
+void display(StackArrayList s){
+ 	if (s.top == -1) {
+        printf("Stack is empty\n");
+        return;
+    }
+    printf("Stack elements: ");
+    for (int i = s.top; i >= 0; i--) {
+        printf("%d ", s.data[i]);
+    }
+    printf("\n");
+
+}
+
+void visualize(StackArrayList s){
+
+	 printf("Stack Visualization:\n");
+    for (int i = MAX - 1; i >= 0; i--) {
+        if (i <= s.top) {
+            printf("| %4d |", s.data[i]);
+            if (i == s.top) {
+                printf(" <- Top");
+            }
+            printf("\n");
+        } else {
+            printf("|      |\n");
+        }
+    }
+    printf("-------\n");
+}
+}
+
+bool isEmpty(StackArrayList s){
+ 	return (s.top == -1);
+}
+
+bool isFull(StackArrayList s){
+	return (s.top == MAX - 1);
+}
+
+#endif
